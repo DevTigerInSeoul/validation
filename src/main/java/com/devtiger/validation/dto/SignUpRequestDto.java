18bf27ca2e7d29b48f187d3076fe13f4f95f3940
pc-999 <test@gmail.com>
2023-08-25 17:41:02 +0900

    1-2. Bean Validation 으로 유효성 검사 해보기

--- src/main/java/com/devtiger/validation/dto/SignUpRequestDto.java
+++ src/main/java/com/devtiger/validation/dto/SignUpRequestDto.java
@@ -2,12 +2,17 @@ package com.devtiger.validation.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class SignUpRequestDto {
 
+    @NotEmpty
     String email;
 
+    @NotEmpty
     String password;
 
+    @NotEmpty
     String nickname;
 }
