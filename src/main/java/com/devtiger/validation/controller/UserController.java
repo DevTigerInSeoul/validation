c3281e669b444bdacf78667ec81f6712c487358e
DevTigerInSeoul <143054847+DevTigerInSeoul@users.noreply.github.com>
2023-08-28 11:38:36 +0900

    1-3 직접 bindingResult에 error추가하기

--- src/main/java/com/devtiger/validation/controller/UserController.java
+++ src/main/java/com/devtiger/validation/controller/UserController.java
@@ -4,6 +4,8 @@ import com.devtiger.validation.dto.SignUpRequestDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -29,7 +31,17 @@ public class UserController {
 
     public String signUp(@Validated @ModelAttribute SignUpRequestDto signUpRequestDto, BindingResult bindingResult, Model model) {
 
+
+        if(true){//내가 작성한 오류가 true라면
+            //bindingResult.addError(new FieldError("signUpRequestDto","email","이메일 중복 입니다."));
+            //bindingResult.addError(new ObjectError("signUpRequestDto","이메일 중복입니다."));
+
+            bindingResult.rejectValue("email","duplicationCheckEmail","이메일 중복입니다.");
+            bindingResult.reject("duplicationCheckEmail","이메일 중복입니다.");
+        }
+
         if(bindingResult.hasErrors()){
+
 
             return "signup";
         }
